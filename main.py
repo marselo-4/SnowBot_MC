4421a986f6642d9bfd17f24ccb7386977bf54933
bazoca333 <alvaromillanes33@gmail.com>
2023-11-29 18:00:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print("hola")
+import pyautogui
+import time
+
+# Definir las posiciones de los diferentes elementos en la pantalla (coordenadas x, y)
+# Estos valores deben ser ajustados según la resolución de tu pantalla y la disposición en el juego
+# Ejemplo: 
+# POSICION_NIEVE = (100, 200)
+# POSICION_VENDER_NPC = (300, 400)
+
+# Función para comprar nieve automáticamente
+def comprar_nieve():
+    # Hacer clic en la posición para comprar nieve
+    pyautogui.click(POSICION_NIEVE)
+    # Esperar un tiempo para que se complete la compra
+    time.sleep(5)  # Ajusta este tiempo según la velocidad de tu conexión y la respuesta del juego
+
+# Función para vender nieve a un NPC automáticamente
+def vender_nieve():
+    # Hacer clic en la posición para vender a un NPC
+    pyautogui.click(POSICION_VENDER_NPC)
+    # Esperar un tiempo para que se complete la venta
+    time.sleep(5)  # Ajusta este tiempo según la velocidad de tu conexión y la respuesta del juego
+
+# Lógica principal del bot
+while True:
+    # Verificar si el pedido de nieve está lleno
+    # Código para verificar la condición de llenado del pedido de nieve
+    
+    # Si el pedido está lleno, vender la nieve
+    if pedido_de_nieve_lleno:
+        vender_nieve()
+    else:
+        # Si el pedido no está lleno, comprar más nieve
+        comprar_nieve()
+    
+    # Esperar un tiempo antes de repetir el ciclo (ajusta según sea necesario)
+    time.sleep(60)  # Esperar un minuto antes de realizar la siguiente acción
